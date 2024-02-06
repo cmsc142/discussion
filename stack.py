2300c73fd267962be35303b7cdf72505d1a4c260
echoumcp1 <101858583+echoumcp1@users.noreply.github.com>
2024-02-05 20:59:32 -0500

    stack.py

---
+++ stack.py
@@ -0,0 +1,24 @@
+from copy import deepcopy
+
+class StatelessStack:
+    def __init__(self, stack):
+        self.stack = stack
+
+    def push(self, item):
+        cpy = deepcopy(self.stack)
+        cpy.append(item)
+        return None, StatelessStack(cpy)
+
+    def pop(self):
+        cpy = deepcopy(self.stack)
+        res = cpy.pop()
+        return res, StatelessStack(cpy)
+
+start = StatelessStack([])
+_, stack1 = start.push(1)
+_, stack2 = stack1.push(2)
+res, stack3 = stack2.pop()
+
+print(stack1.stack)
+print(stack2.stack)
+print(res)
