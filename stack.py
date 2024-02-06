6e7989006fcf0ffe8c42cc23cfb8275a8e20d6c9
echoumcp1 <101858583+echoumcp1@users.noreply.github.com>
2024-02-06 12:32:18 -0500

    discussion updates

--- stack.py
+++ stack.py
@@ -7,11 +7,13 @@ class StatelessStack:
     def push(self, item):
         cpy = deepcopy(self.stack)
         cpy.append(item)
+
         return None, StatelessStack(cpy)
 
     def pop(self):
         cpy = deepcopy(self.stack)
         res = cpy.pop()
+
         return res, StatelessStack(cpy)
 
 start = StatelessStack([])
@@ -19,6 +21,11 @@ _, stack1 = start.push(1)
 _, stack2 = stack1.push(2)
 res, stack3 = stack2.pop()
 
+starter = []
+starter.append(1)
+starter.append(2)
+starter.pop()
+
 print(stack1.stack)
 print(stack2.stack)
-print(res)
+print(stack1.stack)
