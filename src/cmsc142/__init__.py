288b1329fbb89d068642626073ba6dc7f5bc93ad
echoumcp1 <101858583+echoumcp1@users.noreply.github.com>
2024-02-13 07:41:56 -0500

    final edits

--- src/cmsc142/__init__.py
+++ src/cmsc142/__init__.py
@@ -5,7 +5,7 @@ package docstring here
 # .queue represents the subdirectory queue, and within that subdirectory
 # there is another __init__.py that imports all the names from queue.py
 # main benefit is you can directly access StatelessQueue without doing
-# queue.queue.StatelessQueue
+# cmsc142.queue.StatelessQueue
 
 from .misc import *
 from .queue import *
