4ec2550257d505c1331c36ff64ece311753f5edc
echoumcp1 <101858583+echoumcp1@users.noreply.github.com>
2024-03-12 11:00:54 -0400

    added gitignore

--- tests/tests.py
+++ tests/tests.py
@@ -1,11 +1,1 @@
 from cmsc142 import *
-
-def test1():
-    my_tree = Tree()
-    my_tree.insert(-1, "bye")
-    my_tree.insert(-1, "hi")
-    
-    return "hi" == my_tree.search(-1)
-
-if __name__ == "__main__":
-    test1()
