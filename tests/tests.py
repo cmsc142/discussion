d3165249a9a735797d8a8c29e55c31b30f30d6b7
echoumcp1 <101858583+echoumcp1@users.noreply.github.com>
2024-03-14 11:34:19 -0400

    updated to unittest

--- tests/tests.py
+++ tests/tests.py
@@ -1,38 +1,42 @@
+from decimal import DivisionByZero
 from cmsc142 import *
+import unittest
 
-def test1():
-    my_tree = Tree()
+class MyTests(unittest.TestCase):
+    def setUp(self):
+        self.my_tree = Tree()
 
-    my_tree.insert(-1, "bye")
-    my_tree.insert(-1, "hi")
-    return my_tree.search(-1) == "hi"
+    def test0(self):
+        self.my_tree = Tree()
 
-def test2():
-    my_tree = Tree()
+        self.my_tree.insert(-1, "bye")
+        self.assertEqual(self.my_tree.root.value, "bye")
 
-    try:
-        my_tree.search(-1)
-    except Exception as e:
-        return True
+    def test1(self):
+        self.my_tree.insert(-1, "hi")
+        self.assertEqual(self.my_tree.search(-1), "hi")
 
-def postorder(root):
-    if root is None:
-        return ["Lf"]
-    else:
-        return postorder(root.left) + postorder(root.right) + [(root.key, root.value)]
+    def test2(self):
+        my_tree = Tree()
+        
+        with self.assertRaises(Exception):
+            my_tree.search(-1)
 
-def test3():
-    my_tree = Tree()
-    my_tree.insert(5, "5")
-    my_tree.insert(4, "4")
-    my_tree.insert(6, "6")
-    my_tree.insert(3, "3")
+    def test3(self):
+        def postorder(root):
+            if root is None:
+                return ["Lf"]
+            else:
+                return postorder(root.left) + postorder(root.right) + [(root.key, root.value)]
+        my_tree = Tree()
+        my_tree.insert(5, "5")
+        my_tree.insert(4, "4")
+        my_tree.insert(6, "6")
+        my_tree.insert(3, "3")
 
-    porder = postorder(my_tree.root)
-    return porder == ["Lf", "Lf", (3, "3"), "Lf", (4, "4"), "Lf", "Lf", (6, "6"), (5, "5")]
+        porder = postorder(my_tree.root)
+        self.assertEqual(porder,["Lf", "Lf", (3, "3"), "Lf", (4, "4"), "Lf", "Lf", (6, "6"), (5, "5")])
 
 if __name__ == "__main__":
-    print(f'test one passed: {test1()}')
-    print(f'test two passed: {test2()}')
-    print(f'test three passed: {test3()}')
+    unittest.main()
     
